dfda116c934aae0b4d62f3d2c02f4e4718e58104
Bibek Chaudhary <bbkchdhry@gmail.com>
2020-07-10 10:25:35 +0545

    socket, connect timeout increased to 2 mins

--- src/main/java/org/bigmart/util/MongoBullkInsert.java
+++ src/main/java/org/bigmart/util/MongoBullkInsert.java
@@ -1,13 +1,13 @@
 package org.bigmart.util;
 
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientURI;
+import com.mongodb.*;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.typesafe.config.Config;
 import org.apache.log4j.Logger;
 import org.bson.Document;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MongoBullkInsert {
@@ -16,7 +16,27 @@ public class MongoBullkInsert {
     private MongoCollection<Document> collection;
 
     public void connect(Config sinkConfig){
-        mongoClient = new MongoClient(new MongoClientURI(sinkConfig.getString("mongoconnection_uri")));
+        MongoCredential creds = MongoCredential.createCredential(
+                sinkConfig.getString("mongo_user"),
+                sinkConfig.getString("mongo_db"),
+                sinkConfig.getString("mongo_pass").toCharArray());
+
+        MongoClientOptions.Builder optionsBuilder = MongoClientOptions.builder();
+
+        optionsBuilder.connectTimeout(120000);
+        optionsBuilder.socketTimeout(120000);
+        optionsBuilder.serverSelectionTimeout(120000);
+
+        MongoClientOptions options = optionsBuilder.build();
+
+        mongoClient = new MongoClient(
+                new ServerAddress(
+                        sinkConfig.getString("mongo_host"),
+                        Integer.parseInt(sinkConfig.getString("mongo_port"))
+                ), creds, options);
+
+//        mongoClient = new MongoClient(new MongoClientURI(sinkConfig.getString("mongoconnection_uri")));
+
         MongoDatabase database = mongoClient.getDatabase(sinkConfig.getString("mongo_db"));
             collection = database.getCollection(sinkConfig.getString("sink_collection"));
     }
